24c6461be04f549e4f17434bf5787be16b62fea1
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-01-24 14:54:52 +0300

    Added arrow synatx

--- 0x00-ES6_basic/2-main.js
+++
@@ -1,6 +0,0 @@
-import getNeighborhoodsList from './2-arrow';
-
-/* eslint new-cap: ["error", { "newIsCap": false }] */
-const neighborhoodsList = new getNeighborhoodsList();
-const res = neighborhoodsList.addNeighborhood('Noe Valley');
-console.log(res);
