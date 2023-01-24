00d79ea35bfc7eb1032656fc4501a8fbf16e8cf6
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-01-24 15:27:27 +0300

    modified:   3-default-parameter.js

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,10 +1,3 @@
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-  if (expansion1989 === undefined) {
-    expansion1989 = 89; // eslint-disable-line no-param-reassign
-  }
-
-  if (expansion2019 === undefined) {
-    expansion2019 = 19; // eslint-disable-line no-param-reassign
-  }
   return initialNumber + expansion1989 + expansion2019;
 }
