79ca5fbeb978ac1a6697b25cb481e20da85ac2b9
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-01-25 23:56:40 +0300

    modified:   2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  /* eslint-disable no-unused-vars */
-  promise.then((promise) => {
-    console.log('Got a response from the API');
+  promise.then((resp = 'Got a response from the API') => {
+    console.log(resp);
     return { status: 200, body: 'success' };
   }).catch((err) => {
     console.error(err);
