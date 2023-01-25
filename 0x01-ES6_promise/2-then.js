cb3374950e0b5a2f8bcf65919123ddf79c89bb95
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-01-26 00:10:10 +0300

    Edited error

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,7 +3,5 @@ export default function handleResponseFromAPI(promise) {
   promise.then((value) => {
     console.log('Got a response from the API');
     return { status: 200, body: 'success' };
-  }).catch((err) => {
-    console.error(err);
-  });
+  }).catch(new Error());
 }
