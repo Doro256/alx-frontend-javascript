bf949fc69f70cdcb6aadbb24880a54005e35619c
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-01-26 13:22:30 +0300

    modified:   2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,41 +1,45 @@
 class HolbertonCourse {
-    constructor(name, length, students) {
-        if (typeof name !== 'string') {
-            throw new Error ('Name must be a string');
-        }
-
-        if (!Number.isInteger(length) || (length < 0)) {
-            throw new Error ('Length must be a number')
-        }
-
-        if (!Array.isArray(students)) {
-            throw new Error ('Students must be array of Strings')
-        } 
-
-        this._name = name;
-        this._length = length;
-        this._students = students;
-        } 
-
-        get name() {
-            return this._name;
-        }
-        set name(newName) {
-            this._name = newName 
-        }
-        get length() {
-            return this._length;
-        }
-        set length(newLength) {
-            this._length = newLength
-        }
-        get students() {
-            return this._students;
-        }
-        set students(newStudent) {
-            this._students = newStudent
-        }
-      
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
-    
-    export default HolbertonCourse;
+
+    if (!Number.isInteger(length) || (length < 0)) {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be array of Strings');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudent) {
+    this._students = newStudent;
+  }
+}
+
+export default HolbertonCourse;
