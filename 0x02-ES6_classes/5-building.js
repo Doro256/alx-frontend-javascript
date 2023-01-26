58148d527b32247ad3f543bb645d3034befc4bf6
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-01-26 16:58:41 +0300

    Abstract class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,13 @@
 class Building {
-    constructor(sqft) {
-        if (new.target === Building) {
-            throw new Error ('Class extending Building must override evacuationWarningMessage');
-        }
-        this._sqft = sqft;
+  constructor(sqft) {
+    if (new.target === Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    get sqft() {
-        return this._sqft;
-    }
-   
-    evacuationWarningMessage() {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
 }
- export default Building;
+export default Building;
