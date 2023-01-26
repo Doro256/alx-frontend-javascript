f1716d7a0e6fd5f3287e184429a31a1097fd87ff
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-01-26 18:29:53 +0300

    modified:   5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,16 @@
 class Building {
-  constructor(sqft) {
-    this._sqft = sqft;
-    if (new.target !== Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    constructor(sqft) {
+      this._sqft = sqft;
+      if (new.target !== Building) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+      if ((new.target !== Building) && !Object.getOwnPropertyNames(Object.getPrototypeOf(this)).includes('evacuationWarningMessage')) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
-    if ((new.target !== Building) && !Object.getOwnPropertyNames(Object.getPrototypeOf(this)).includes('evacuationWarningMessage')) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+  
+    get sqft() {
+      return this._sqft;
     }
   }
-
-  get sqft() {
-    return this._sqft;
-  }
-}
-export default Building;
+  export default Building;
