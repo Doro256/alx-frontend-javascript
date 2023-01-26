41a5a6c30e3709d131955f4f07e8fb6dd57cbd50
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-01-26 18:31:38 +0300

    Fixed linter

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,16 @@
 class Building {
-    constructor(sqft) {
-      this._sqft = sqft;
-      if (new.target !== Building) {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
-      if ((new.target !== Building) && !Object.getOwnPropertyNames(Object.getPrototypeOf(this)).includes('evacuationWarningMessage')) {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (new.target !== Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-  
-    get sqft() {
-      return this._sqft;
+    if ((new.target !== Building) && !Object.getOwnPropertyNames(Object.getPrototypeOf(this)).includes('evacuationWarningMessage')) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
-  export default Building;
+
+  get sqft() {
+    return this._sqft;
+  }
+}
+export default Building;
