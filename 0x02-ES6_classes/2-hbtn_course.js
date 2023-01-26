a477dd0cc2cfa78b9364a735ac6d0673e5792ada
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-01-26 13:18:16 +0300

    Implement a class named HolbertonCourse

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,41 @@
+class HolbertonCourse {
+    constructor(name, length, students) {
+        if (typeof name !== 'string') {
+            throw new Error ('Name must be a string');
+        }
+
+        if (!Number.isInteger(length) || (length < 0)) {
+            throw new Error ('Length must be a number')
+        }
+
+        if (!Array.isArray(students)) {
+            throw new Error ('Students must be array of Strings')
+        } 
+
+        this._name = name;
+        this._length = length;
+        this._students = students;
+        } 
+
+        get name() {
+            return this._name;
+        }
+        set name(newName) {
+            this._name = newName 
+        }
+        get length() {
+            return this._length;
+        }
+        set length(newLength) {
+            this._length = newLength
+        }
+        get students() {
+            return this._students;
+        }
+        set students(newStudent) {
+            this._students = newStudent
+        }
+      
+    }
+    
+    export default HolbertonCourse;
