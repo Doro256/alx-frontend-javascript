0cc06b355d2cd791a60d090ea41a2f613cf2adf6
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-01-31 20:19:06 +0300

    modified:   10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,3 +1,6 @@
 export default function updateUniqueItems(groceriesList) {
-  return groceriesList.set('Pasta', 100).set('Rice', 100);
+  if (groceriesList instanceof Map) {
+    return groceriesList.set('Pasta', 100).set('Rice', 100);
+  }
+  throw TypeError('Cannot process');
 }
