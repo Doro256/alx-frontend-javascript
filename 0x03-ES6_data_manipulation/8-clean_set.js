89b771adc2f9f771d8f1fed6924613494e5d26a0
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-01-31 18:11:44 +0300

    returns a string of all the set values that start with a specific string

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+    if (!startString)
+    return '';
+    
+    let result = '';
+    set.forEach(element => {
+        if(element.startsWith(startString)) {
+            result += element.substring(startString.length) + '-';
+        }
+    });
+    return result.slice(0, -1);
+}
