9c8e45d12aaf98ee4653e240fe935b02ca2abcba
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-01-31 19:16:55 +0300

    modified:   9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,13 +1,7 @@
 export default function groceriesList() {
-  const obj = {
-    Apples: 10,
-    Tomatoes: 10,
-    Pasta: 1,
-    Rice: 1,
-    Banana: 5,
-  };
-
-  const newArray = Object.entries(obj);
-
-  return new Map(newArray);
+  return new Map([['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5]]);
 }
