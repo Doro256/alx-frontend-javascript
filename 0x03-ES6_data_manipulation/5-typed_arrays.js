33ebe3e924ec164bdf1705d1efd82bbe5869b2d5
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-01-31 14:40:18 +0300

    Typed Array

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+export default function createInt8TypedArray(length, position, value) {
+    if (position >= length || position < 0 ) {
+        throw new Error ('Position outside range');
+    }
+        
+    const buffer = new ArrayBuffer(length);
+    const arr = new Int8Array(buffer);
+    const view = new DataView(buffer, 0, length);
+    arr[position] = value;
+   
+   return view;
+}
