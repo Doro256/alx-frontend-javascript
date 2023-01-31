e566545c8ef6cd2684b7948d1e8ab67f1b2819fa
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-01-31 18:21:31 +0300

    modified:   8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (!startString) { return ''; }
-
   let result = '';
-  set.forEach((element) => {
-    if (element.startsWith(startString)) {
-      result += `${element.substring(startString.length)}-`;
-    }
-  });
-  return result.slice(0, -1);
+  if (startString && typeof startString === 'string') {
+    set.forEach((element) => {
+      if (element.startsWith(startString)) {
+        result += `${element.substring(startString.length)}-`;
+      }
+    });
+    return result.slice(0, -1);
+  }
+  return result;
 }
